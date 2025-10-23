7e25dcc05ab9cc43c4c7b244417552b5700247e7
birdh <birdh@BANANNACOMPUTE>
2025-10-23 13:21:10 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+/*
+* File: Lab 8
+* Author: Anna Keeler
+* Date: 10/23/2025
+*
+* Description: 8.1 Commit project
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main(void) {
+   
+           
+                    
+
+                return 0;
+            }
+
